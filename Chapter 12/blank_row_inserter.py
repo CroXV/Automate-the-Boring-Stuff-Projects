7ca31f5af8c98b9d444e8209867eaa6fc201efc6
CroXV <CroXV001@gmail.com>
2019-08-17 17:36:28 +0530

    minor changes

--- Chapter 12/blank_row_inserter.py
+++ Chapter 12/blank_row_inserter.py
@@ -10,15 +10,16 @@ if len(sys.argv) == 4:
     index_row = int(sys.argv[1])
     num_rows = int(sys.argv[2])
 
-    print('Selecting file...')
+    print('Loading file...')
     wb = openpyxl.load_workbook(file)
     ws = wb.active
-    print('Adding rows...')
+
+    print('Adding blank rows...')
     for _ in range(num_rows):
         ws.insert_rows(index_row)
 
     print('Saving file...')
     wb.save(sys.argv[3])
-    print('Done.')
+    print(f'\n\t{"-"*3} Done {"-"*3}')
 else:
     raise Exception('Not enough arguments.')
