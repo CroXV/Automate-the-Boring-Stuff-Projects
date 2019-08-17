591cc787a1d733dfe9c3a09911225c2f34d0185f
CroXV <CroXV001@gmail.com>
2019-08-17 13:59:36 +0530

    freezed column A, modified comments

--- Chapter 12/multiplication_table.py
+++ Chapter 12/multiplication_table.py
@@ -9,17 +9,20 @@ import sys
 if len(sys.argv) == 2:
     multiply_num = int(sys.argv[1]) + 2
 else:
-    multiply_num = int(input('Multiplication Number:\n> ')) + 2
+    multiply_num = int(input('Multiplication Table Number:\n> ')) + 2
 
-# Create new workbook
+# create new workbook
 wb = openpyxl.Workbook()
 ws = wb.active
+# freeze column A
+ws.freeze_panes = 'B1'
+# create new font settings
 bold = Font(bold=True)
 
+# skip cell A1
 for num in range(2, multiply_num):
-    # first row without a1 formatted to bold
+    # format first column and row to bold
     ws.cell(row=1, column=num, value=num - 1).font = bold
-    # first column without a1 formatted to bold
     ws.cell(row=num, column=1, value=num - 1).font = bold
     for col in range(2, multiply_num):
         ws.cell(row=num, column=col, value=(num - 1) * (col - 1))
