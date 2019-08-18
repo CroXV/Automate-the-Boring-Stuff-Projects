92ead2b1c20d14e2d10cfb9831f1ba6821529170
CroXV <CroXV001@gmail.com>
2019-08-18 14:06:08 +0530

    added option to select workbook

--- Chapter 12/spreadsheet_to_text_files.py
+++ Chapter 12/spreadsheet_to_text_files.py
@@ -1,10 +1,17 @@
 #! /usr/bin/env python
+# Usage: py.exe spreadsheet_to_text_files.py <workbook>
 
 from openpyxl.utils import get_column_letter as gcl
 import openpyxl
+import sys
 
 
-wb = openpyxl.load_workbook('(inverted).xlsx')
+if len(sys.argv) == 2:
+    file = sys.argv[1]
+else:
+    file = input('Enter workbook path:\n> ')
+
+wb = openpyxl.load_workbook(file)
 ws = wb.active
 
 
